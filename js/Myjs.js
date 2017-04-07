e29f0cc721732192acc315c9ec9fef84b1867b98
jspahlin <jspahlin@kent.edu>
2017-04-07 15:22:43 -0400

    fix typo and check for undefined data being passed in.

--- js/Myjs.js
+++ js/Myjs.js
@@ -164,9 +164,11 @@ function testVis(data) {
 //preparse data to reduce it to values without undefined values.
 	cleanData = function (d) {
 		var newArray = new Array();
-		for (var i = 0; i < 10; ++i) {
-			if (d[i].duration != null && d[i].avgspeed != null) {
-				newArray.push(d[i]);
+		if(d != null) {
+			for (var i = 0; i < d.length; ++i) {
+				if (d[i].duration != null && d[i].avspeed != null) {
+					newArray.push(d[i]);
+				}
 			}
 		}
 		return newArray;
@@ -191,7 +193,7 @@ var y = d3.scaleLinear()
 
 var line = d3.line()
     .x(function(d) { return x(d.duration); })
-    .y(function(d) { return y(d.avgspeed); });
+    .y(function(d) { return y(d.avspeed); });
 
 /*d3.tsv("js/data.tsv", function(d) {
   d.date = parseTime(d.date);
@@ -205,7 +207,7 @@ var line = d3.line()
 	console.log("before x.domain");
   x.domain(d3.extent(data, function(d) { return d.duration; }));
 	console.log("before y.domain");
-  y.domain(d3.extent(data, function(d) { return d.avgspeed; })); 
+  y.domain(d3.extent(data, function(d) { return d.avspeed; })); 
 
 	console.log("before g.append(\"g\")");
   g.append("g")
