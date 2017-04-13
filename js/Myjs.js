7d2b504fc4d1a87a61b0df24eb0774c977485c05
Michael Capriles <Michael.Capriles@Live.com>
2017-04-13 11:56:51 -0400

    Got mouseover on barchart working

--- js/Myjs.js
+++ js/Myjs.js
@@ -4,140 +4,125 @@
 var lat = 41.141376;
 var lng = -8.613999;
 var zoom = 14;
-
 // add an OpenStreetMap tile layer
 var mbAttr = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-    '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-    'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    mbUrl = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoicGxhbmVtYWQiLCJhIjoiemdYSVVLRSJ9.g3lbg_eN0kztmsfIPxa9MQ';
-
-
+'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+mbUrl = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoicGxhbmVtYWQiLCJhIjoiemdYSVVLRSJ9.g3lbg_eN0kztmsfIPxa9MQ';
 var grayscale = L.tileLayer(mbUrl, {
-        id: 'mapbox.light',
-        attribution: mbAttr
-    }),
-    streets = L.tileLayer(mbUrl, {
-        id: 'mapbox.streets',
-        attribution: mbAttr
-    });
-
-
-var map = L.map('map', {
-    center: [lat, lng], // Porto
-    zoom: zoom,
-    layers: [streets],
-    zoomControl: true,
-    fullscreenControl: true,
-    fullscreenControlOptions: { // optional
-        title: "Show me the fullscreen !",
-        titleCancel: "Exit fullscreen mode",
-        position: 'bottomright'
-    }
+	id: 'mapbox.light',
+	attribution: mbAttr
+}),
+streets = L.tileLayer(mbUrl, {
+	id: 'mapbox.streets',
+	attribution: mbAttr
 });
-
+var map = L.map('map', {
+	center: [lat, lng], // Porto
+	zoom: zoom,
+	layers: [streets],
+	zoomControl: true,
+	fullscreenControl: true,
+	fullscreenControlOptions: { // optional
+		title: "Show me the fullscreen !",
+		titleCancel: "Exit fullscreen mode",
+		position: 'bottomright'
+	}
+});
 var baseLayers = {
-    "Grayscale": grayscale, // Grayscale tile layer
-    "Streets": streets, // Streets tile layer
+	"Grayscale": grayscale, // Grayscale tile layer
+	"Streets": streets, // Streets tile layer
 };
-
 layerControl = L.control.layers(baseLayers, null, {
-    position: 'bottomleft'
+	position: 'bottomleft'
 }).addTo(map);
-
 // Initialise the FeatureGroup to store editable layers
 var drawnItems = new L.FeatureGroup();
 map.addLayer(drawnItems);
-
 var featureGroup = L.featureGroup();
-
 var drawControl = new L.Control.Draw({
-    position: 'bottomright',
+	position: 'bottomright',
 	collapsed: false,
-    draw: {
-        // Available Shapes in Draw box. To disable anyone of them just convert true to false
-        polyline: false,
-        polygon: false,
-        circle: false,
-        rectangle: true,
-        marker: false,
-    },
-    edit: {
-        featureGroup: drawnItems,
-        //remove: true,
-        //edit: true
-    }
-
+	draw: {
+		// Available Shapes in Draw box. To disable anyone of them just convert true to false
+		polyline: false,
+		polygon: false,
+		circle: false,
+		rectangle: true,
+		marker: false,
+	},
+	edit: {
+		featureGroup: drawnItems,
+		//remove: true,
+		//edit: true
+	}
 });
 map.addControl(drawControl); // To add anything to map, add it to "drawControl"
 //*******************************************************************************************************************************************************
 //*****************************************************************************************************************************************
 // Index Road Network by Using R-Tree
 //*****************************************************************************************************************************************
-var rt = cw(function(data,cb){
+var rt = cw(function (data, cb) {
 	var self = this;
-	var request,_resp;
+	var request, _resp;
 	importScripts("js/rtree.js");
-	if(!self.rt){
-		self.rt=RTree();
+	if (!self.rt) {
+		self.rt = RTree();
 		request = new XMLHttpRequest();
 		request.open("GET", data);
-		request.onreadystatechange = function() {
+		request.onreadystatechange = function () {
 			if (request.readyState === 4 && request.status === 200) {
-				_resp=JSON.parse(request.responseText);
+				_resp = JSON.parse(request.responseText);
 				self.rt.geoJSON(_resp);
 				cb(true);
 			}
 		};
 		request.send();
-	}else{
+	} else {
 		return self.rt.bbox(data);
 	}
 });
-
 rt.data(cw.makeUrl("js/trips.json"));
 //*****************************************************************************************************************************************
 // Clear the Map.
-//*****************************************************************************************************************************************	
+//***************************************************************************************************************************************** 
 function clearMap() {
-    for (i in map._layers) {
-        if (map._layers[i]._path != undefined) {
-            try {
-                map.removeLayer(map._layers[i]);
-            } catch (e) {
-                console.log("problem with " + e + map._layers[i]);
-            }
-        }
-    }
+	for (i in map._layers) {
+		if (map._layers[i]._path != undefined) {
+			try {
+				map.removeLayer(map._layers[i]);
+			} catch (e) {
+				console.log("problem with " + e + map._layers[i]);
+			}
+		}
+	}
 }
 //*****************************************************************************************************************************************
 // Draw rectangle on Map Event for Query :
 // Click the small box on Map and start drawing to do query.
-//*****************************************************************************************************************************************	
+//***************************************************************************************************************************************** 
 var lineData;
 var barData;
 map.on('draw:created', function (e) {
-	
 	clearMap();
-	
 	var type = e.layerType,
-		layer = e.layer;
-	
+	layer = e.layer;
 	if (type === 'rectangle') {
-		var bounds=layer.getBounds();
-		rt.data([[bounds.getSouthWest().lng,bounds.getSouthWest().lat],[bounds.getNorthEast().lng,bounds.getNorthEast().lat]]).
-		then(function(d){var result = d.map(function(a) {return a.properties;});
-				 //console.log(d.length);
-		barData= result;
-		lineData = result;
-		console.log(result);		// Trip Info: avspeed, distance, duration, endtime, maxspeed, minspeed, starttime, streetnames, taxiid, tripid
-				 		// update graphs when drawing a rectangle
-		testVis(lineData);
-		barChart(barData);
-		DrawRS(result);
-		});
-
+		var bounds = layer.getBounds();
+		rt.data([[bounds.getSouthWest().lng, bounds.getSouthWest().lat], [bounds.getNorthEast().lng, bounds.getNorthEast().lat]]).
+        then(function (d) {
+        	var result = d.map(function (a) { return a.properties; });
+        	//console.log(d.length);
+        	barData = result;
+        	lineData = result;
+        	console.log(result); // Trip Info: avspeed, distance, duration, endtime, maxspeed, minspeed, starttime, streetnames, taxiid, tripid
+        	// update graphs when drawing a rectangle
+        	testVis(lineData);
+        	barChart(barData);
+        	DrawRS(result);
+        });
 	}
-	drawnItems.addLayer(layer);			//Add your Selection to Map  
+	drawnItems.addLayer(layer); //Add your Selection to Map 
 	//console.log("hi I made a change!");
 });
 //*****************************************************************************************************************************************
@@ -145,164 +130,148 @@ map.on('draw:created', function (e) {
 // Input is a list of Trip and the function draw these trips on Map based on their IDs
 //*****************************************************************************************************************************************
 function DrawRS(trips) {
-	for (var j=0; j<trips.length; j++) {  // Check Number of Segments and go through all segments
-		var TPT = new Array();			  
-		TPT = TArr[trips[j].tripid].split(',');  		 // Find each segment in TArr Dictionary. 
+	for (var j = 0; j < trips.length; j++) { // Check Number of Segments and go through all segments
+		var TPT = new Array();
+		TPT = TArr[trips[j].tripid].split(','); // Find each segment in TArr Dictionary. 
 		var polyline = new L.Polyline([]).addTo(drawnItems);
-        polyline.setStyle({
-            color: 'red',                      // polyline color
-			weight: 1,                         // polyline weight
-			opacity: 0.5,                      // polyline opacity
-			smoothFactor: 1.0  
-        });
-		for(var y = 0; y < TPT.length-1; y=y+2){    // Parse latlng for each segment
-			polyline.addLatLng([parseFloat(TPT[y+1]), parseFloat(TPT[y])]);
+		polyline.setStyle({
+			color: 'red', // polyline color
+			weight: 1, // polyline weight
+			opacity: 0.5, // polyline opacity
+			smoothFactor: 1.0
+		});
+		for (var y = 0; y < TPT.length - 1; y = y + 2) { // Parse latlng for each segment
+			polyline.addLatLng([parseFloat(TPT[y + 1]), parseFloat(TPT[y])]);
 		}
-	}		
+	}
 }
 // inspiration taken from https://bl.ocks.org/mbostock/raw/3883245/
 // for some of the linegraph code.
-
 var testVisData;
 function cleanData(d) {
-	
-		var newArray = new Array();
-		//if (d == null) { return newArray; }
-		for (var i = 0; i < d.length; ++i) {
-			if (d[i].duration != null && d[i].avspeed != null) {
-				newArray.push(d[i]);
-			}
+	var newArray = new Array();
+	//if (d == null) { return newArray; }
+	for (var i = 0; i < d.length; ++i) {
+		if (d[i].duration != null && d[i].avspeed != null) {
+			newArray.push(d[i]);
 		}
-	//console.log(newArray);
-		return newArray;
 	}
+	//console.log(newArray);
+	return newArray;
+}
 function testVis(data) {
-//preparse data to reduce it to values without undefined values.
-	if(data == null) { return null; }
-	
+	//preparse data to reduce it to values without undefined values.
+	if (data == null) { return null; }
 	testVisData = cleanData(data);
 	data = testVisData;
-
 	data = data.sort(function (a, b) {
 		return a.duration - b.duration;
 	});
-	
 	d3.select("body").select("div#rightside").select("div#linechart").selectAll("*").remove();
-var svg = d3.select("body").select("div#rightside").select("div#linechart").append("svg").attr("width", 400).attr("height",300).attr("id","testVis"),
-    margin = {top: 20, right: 20, bottom: 30, left: 50},
-    width = +svg.attr("width") - margin.left - margin.right,
-    height = +svg.attr("height") - margin.top - margin.bottom,
-    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")").on("click", function(d) { console.log(d); });
-
-var x = d3.scaleTime()
-    .rangeRound([0, width]);
-
-var y = d3.scaleLinear()
-    .rangeRound([height, 0]);
-
-var line = d3.line()
-    .x(function(d) { return x(d.duration); })
-    .y(function(d) { return y(d.avspeed); });
-
+	var svg = d3.select("body").select("div#rightside").select("div#linechart").append("svg").attr("width", 400).attr("height", 300).attr("id", "testVis"),
+	margin = { top: 20, right: 20, bottom: 30, left: 50 },
+	width = +svg.attr("width") - margin.left - margin.right,
+	height = +svg.attr("height") - margin.top - margin.bottom,
+	g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")").on("click", function (d) { console.log(d); });
+	var x = d3.scaleTime()
+		.rangeRound([0, width]);
+	var y = d3.scaleLinear()
+		.rangeRound([height, 0]);
+	var line = d3.line()
+		.x(function (d) { return x(d.duration); })
+		.y(function (d) { return y(d.avspeed); });
 	// we handle data that is passed in the first argument of the function.
 	// avgspeed, distance, duration, endtime, maxspeed, minspeed, starttime, streetnames{...}, taxiid, tripid.
-	
-  x.domain(d3.extent(data, function(d) { return d.duration; }));	
-  y.domain(d3.extent(data, function(d) { return d.avspeed; })); 
-
-  g.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x))
-    .select(".domain")
-      .remove();
-	
-  g.append("g")
-      .call(d3.axisLeft(y))
-    .append("text")
-      .attr("fill", "#000")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", "0.71em")
-      .attr("text-anchor", "end")
-      .text("Price ($)");
-
-  g.append("path")
-      .datum(data)
-      .attr("fill", "none")
-      .attr("stroke", "steelblue")
-      .attr("stroke-linejoin", "round")
-      .attr("stroke-linecap", "round")
-      .attr("stroke-width", 1.5)
-      .attr("d", line);
-
+	x.domain(d3.extent(data, function (d) { return d.duration; }));
+	y.domain(d3.extent(data, function (d) { return d.avspeed; }));
+	g.append("g")
+		.attr("transform", "translate(0," + height + ")")
+		.call(d3.axisBottom(x))
+		.select(".domain")
+		.remove();
+	g.append("g")
+		.call(d3.axisLeft(y))
+		.append("text")
+		.attr("fill", "#000")
+		.attr("transform", "rotate(-90)")
+		.attr("y", 6)
+		.attr("dy", "0.71em")
+		.attr("text-anchor", "end")
+		.text("Price ($)");
+	g.append("path")
+		.datum(data)
+		.attr("fill", "none")
+		.attr("stroke", "steelblue")
+		.attr("stroke-linejoin", "round")
+		.attr("stroke-linecap", "round")
+		.attr("stroke-width", 1.5)
+		.attr("d", line);
 }
-
 // sample bar chart
-
-function barChart(data){
+function barChart(data) {
 	data = cleanData(data);
-	
-	var margin = {top: 20, right: 20, bottom: 70, left: 40},
-    width = 400 - margin.left - margin.right,
-    height = 300 - margin.top - margin.bottom;
+	var margin = { top: 20, right: 20, bottom: 70, left: 40 },
+	width = 400 - margin.left - margin.right,
+	height = 300 - margin.top - margin.bottom;
 	for (var i = 0; i < data.length; ++i) {
-			if (data[i].avspeed != null && data[i].duration != null) {
-				data[i].avspeed = Math.round(data[i].avspeed);
-				data[i].duration = Math.round(data[i].duration);
-			}
+		if (data[i].avspeed != null && data[i].duration != null) {
+			data[i].avspeed = Math.round(data[i].avspeed);
+			data[i].duration = Math.round(data[i].duration);
 		}
-data = data.slice(1,20);
+	}
+	data = data.slice(1, 20);
 	data = data.sort(function (a, b) {
 		return a.avspeed - b.avspeed;
 	});
-
-var x = d3.scaleBand().rangeRound([0, width]).padding(0.2);
-var y = d3.scaleLinear().range([height, 0]);
-
-var xAxis = d3.axisBottom(x).ticks(10);
-var yAxis = d3.axisLeft(y).ticks(10);
-	
-d3.select("body").select("div#rightside").select("div#barchart").selectAll("*").remove();
-
-var svg = d3.select("body").select("div#rightside").select("div#barchart").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", 
-          "translate(" + margin.left + "," + margin.top + ")");
-		 	
-  //x.domain(d3.extent(data, function(d) { return d.avspeed; }));
-  x.domain(data.map(function(d) { return d.avspeed; }));
-  y.domain(d3.extent(data, function(d) { return d.duration;  }));
-
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis)
-      .selectAll("text")
-      .style("text-anchor", "end")
-      .attr("dx", "-.8em")
-      .attr("dy", "-.55em")
-      .attr("transform", "rotate(-90)" );
-
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-      .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Duration")//"Value ($)")
-	  .attr("fill", "red");
-
-  svg.selectAll("bar")
-      .data(data)
-      .enter().append("rect")
-      .style("fill", "steelblue")
-      .attr("x", function(d) { console.log([d.avspeed,d.duration]); return x(d.avspeed); })
-      .attr("width", x.bandwidth())
-      .attr("y", function(d) { return y(d.duration); })
-      .attr("height", function(d) { return height - y(d.duration); });
+	var x = d3.scaleBand().rangeRound([0, width]).padding(0.2);
+	var y = d3.scaleLinear().range([height, 0]);
+	var xAxis = d3.axisBottom(x).ticks(10);
+	var yAxis = d3.axisLeft(y).ticks(10);
+	d3.select("body").select("div#rightside").select("div#barchart").selectAll("*").remove();
+	var svg = d3.select("body").select("div#rightside").select("div#barchart").append("svg")
+		.attr("width", width + margin.left + margin.right)
+		.attr("height", height + margin.top + margin.bottom)
+		.append("g")
+		.attr("transform",
+	"translate(" + margin.left + "," + margin.top + ")");
+	//x.domain(d3.extent(data, function(d) { return d.avspeed; }));
+	x.domain(data.map(function (d) { return d.avspeed; }));
+	y.domain(d3.extent(data, function (d) { return d.duration; }));
+	svg.append("g")
+		.attr("class", "x axis")
+		.attr("transform", "translate(0," + height + ")")
+		.call(xAxis)
+		.selectAll("text")
+		.style("text-anchor", "end")
+		.attr("dx", "-.8em")
+		.attr("dy", "-.55em")
+		.attr("transform", "rotate(-90)");
+	svg.append("g")
+		.attr("class", "y axis")
+		.call(yAxis)
+		.append("text")
+		.attr("transform", "rotate(-90)")
+		.attr("y", 6)
+		.attr("dy", ".71em")
+		.style("text-anchor", "end")
+		.text("Duration")//"Value ($)")
+		.attr("fill", "red");
+	svg.selectAll("bar")
+		.data(data)
+		.enter().append("rect")
+		.style("fill", "steelblue")
+		.attr("x", function (d) { console.log([d.avspeed, d.duration]); return x(d.avspeed); })
+		.attr("width", x.bandwidth())
+		.attr("y", function (d) { return y(d.duration); })
+		.attr("height", function (d) { return height - y(d.duration); })
+		.on("mouseover", function (d, i) {
+			svg.append("text")
+            .attr("id", "t" + d.tripid)
+            .text(function () {
+            	return ["TaxiID: " + d.taxiid, "TripID: " + d.tripid];
+            });
+		})
+		.on("mouseout", function (d, i) {
+			d3.select("#t" + d.tripid).remove();
+		});
 }
-
