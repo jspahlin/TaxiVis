1e5236e3bcf2bb45100a842af9f4ab3b8669b110
jspahlin <jspahlin@kent.edu>
2017-04-14 14:56:06 -0400

    3 out of 5 can show/hide now

--- js/Toolbarjs.js
+++ js/Toolbarjs.js
@@ -1,1 +1,8 @@
 // javascript that handles toolbar on click calls
+var visualizations = ["barchart", "scatterplot", "linechart"];
+function showChart(chart) {
+  for(id in visualizations) {
+    document.getElementById(id).style.display = "none";
+  }
+  document.getElementById(chart).style.display = "block";
+}
