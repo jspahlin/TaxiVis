4e18e70c5952d5b43c5d50e43c39c6984e6c6f45
msultana2 <msultana@kent.edu>
2017-04-14 14:22:00 -0400

    scatterplot added

--- js/Myjs.js
+++ js/Myjs.js
@@ -168,8 +168,11 @@ function clearMap() {
 // Draw rectangle on Map Event for Query :
 // Click the small box on Map and start drawing to do query.
 //***************************************************************************************************************************************** 
+
 var lineData;
 var barData;
+var scatterData;
+
 map.on('draw:created', function (e) {
 	clearMap();
 	var type = e.layerType,
@@ -182,10 +185,14 @@ map.on('draw:created', function (e) {
         	//console.log(d.length);
         	barData = result;
         	lineData = result;
+			scatterData = result;
+			
         	console.log(result); // Trip Info: avspeed, distance, duration, endtime, maxspeed, minspeed, starttime, streetnames, taxiid, tripid
         	// update graphs when drawing a rectangle
-        	testVis(lineData);
+        	//testVis(lineData);
         	barChart(barData);
+			scatterPlot(scatterData);
+			
         	DrawRS(result);
         });
 	}
@@ -342,5 +349,72 @@ function barChart(data) {
 		});
 }
 
+//=========sample scatterplot graph=========
+
+function scatterPlot(data){
+	
+	data = cleanData(data);
+	
+// set the dimensions and margins of the graph
+var margin = {top: 20, right: 20, bottom: 30, left: 50},
+    width = 400 - margin.left - margin.right,
+    height = 300 - margin.top - margin.bottom;
+
+// set the ranges
+var x = d3.scaleTime().range([0, width]);
+var y = d3.scaleLinear().range([height, 0]);
+
+// define the line
+var valueline = d3.line()
+    .x(function(d) { return x(d.duration); })
+    .y(function(d) { return y(d.avspeed); });
+
+// append the svg obgect to the body of the page
+// appends a 'group' element to 'svg'
+// moves the 'group' element to the top left margin
+
+d3.select("body").select("div#rightside").select("div#scatterplot").selectAll("*").remove();
+var svg = d3.select("body").select("div#rightside").select("div#scatterplot").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+	.append("g")
+    .attr("transform","translate(" + margin.left + "," + margin.top + ")");
+
+/* // Get the data
+d3.csv("data.csv", function(error, data) {
+  if (error) throw error;
+  // format the data
+  data.forEach(function(d) {
+      d.date = parseTime(d.date);
+      d.close = +d.close;
+  }); */
+  
+  // scale the range of the data
+  x.domain(d3.extent(data, function(d) { return d.duration; }));
+  y.domain([0, d3.max(data, function(d) { return d.avspeed; })]);
+  
+  // add the valueline path.
+  /*svg.append("path")
+     .data([data])
+     .attr("class", "line")
+     .attr("d", valueline);*/
+  
+  // add the dots
+  svg.selectAll("dot")
+     .data(data)
+     .enter().append("circle")
+     .attr("r", 5)
+     .attr("cx", function(d) { return x(d.duration); })
+     .attr("cy", function(d) { return y(d.avspeed); });
+  
+  // add the X Axis
+  svg.append("g")
+     .attr("transform", "translate(0," + height + ")")
+     .call(d3.axisBottom(x));
+  
+  // add the Y Axis
+  svg.append("g")
+     .call(d3.axisLeft(y));
+}
 
 
